0e8ab2b5ea61d2692a63979003bc3d6718b4e2e3
wpghks <wpghks56@naver.com>
2023-10-10 14:27:49 +0900

    [feature] create kafka producer

---
+++ src/main/java/net/javaguides/kafkademo/kafka/KafkaProducer.java
@@ -0,0 +1,19 @@
+package net.javaguides.kafkademo.kafka;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class KafkaProducer {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        log.info(message);
+        kafkaTemplate.send("javaguides", message);
+    }
+
+}
